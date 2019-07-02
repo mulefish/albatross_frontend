44abd23502975ec592cefcfe35564856e0dce74c
mulefish <finch.wren.swift@gmail.com>
2019-07-02 12:43:28 -0700

    parse sgf for history + who won

--- python_backend/SGFParser.py
+++ python_backend/SGFParser.py
@@ -1,8 +1,20 @@
 # sgf meant 'simple go format' but backronymed to 'simple game format'
 # files = ["testfile.txt"]  # works 
-files = ["069.sgf", "093.sgf"]
+files = ["069.sgf"] # , "093.sgf"]
 
 for sgf in files:
     with open(sgf, encoding='utf-8') as f:
+        accumulator = []
+        result = "" 
         for line in f:
-            print( line)
+            # print( line)
+            if line[0:1] == ";":
+                line = line.replace("\n","")
+                line = line.replace(")","")
+                accumulator.append(line)
+            if line[0:3] == "RE[":
+                result = line.replace("\n","")
+
+        data = "".join(accumulator)
+        print(data)
+        print(result)
